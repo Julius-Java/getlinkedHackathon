9cd16fe204dccd9c6b9a0345be1fd9aa1a5d4402
Emmanuel Julius <emmanueljulius9102016@gmail.com>
2023-09-23 20:42:10 +0100

    Make header fixed on scroll

--- components/shared/Navbar.tsx
+++ components/shared/Navbar.tsx
@@ -22,38 +22,38 @@ const Navbar = () => {
 
 
     return (
-    <nav
-        className='h-[10vh] max-w-7xl w-[90%] mx-auto flex justify-between items-center  relative'
-    >
-
-        <GetLinkedText />
-
-        <Hamburger
-            menuOpen={menuOpen}
-            setMenuOpen={setMenuOpen}
-        />
-        <div
-            className={`absolute ${menuOpen ? "top-0" : "-top-[500px]"} z-20  -left-8 py-10 px-10  w-[120%] sm:left-0 bg-primaryPurpleDark transition-all duration-300 sm:top-0 sm:bg-transparent sm:relative sm:p-0 sm:ml-auto sm:w-auto`}
+    <div className='fixed w-full z-40 top-0 left-0 bg-primaryPurpleDark'>
+        <nav
+            className='h-[10vh] max-w-7xl w-[90%] mx-auto flex justify-between items-center  relative'
         >
-            <ul
-                className={`${inter.className} text-white flex flex-col gap-4 mt-8 sm:flex-row sm:items-center sm:m-0 sm:gap-6 sm:text-sm`}
+            <GetLinkedText />
+            <Hamburger
+                menuOpen={menuOpen}
+                setMenuOpen={setMenuOpen}
+            />
+            <div
+                className={`absolute ${menuOpen ? "top-0" : "-top-[500px]"} z-20  -left-8 py-10 px-10  w-[120%] sm:left-0 bg-primaryPurpleDark transition-all duration-300 sm:top-0 sm:bg-transparent sm:relative sm:p-0 sm:ml-auto sm:w-auto`}
             >
-                {
-                    mainNavLinks.map(({name, href}, _) => (
-                        <Link href={href} key={name}>
-                            {name}
-                        </Link>
-                    ))
-                }
-                <button
-                    className='self-start cta-btn mt-4 md:ml-6 sm:m-0 xl:py-4 xl:px-12'
-                    onClick={() => router.push('/register')}
+                <ul
+                    className={`${inter.className} text-white flex flex-col gap-4 mt-8 sm:flex-row sm:items-center sm:m-0 sm:gap-6 sm:text-sm`}
                 >
-                        Register
-                </button>
-            </ul>
-        </div>
-    </nav>
+                    {
+                        mainNavLinks.map(({name, href}, _) => (
+                            <Link href={href} key={name}>
+                                {name}
+                            </Link>
+                        ))
+                    }
+                    <button
+                        className='self-start cta-btn mt-4 md:ml-6 sm:m-0 xl:py-4 xl:px-12'
+                        onClick={() => router.push('/register')}
+                    >
+                            Register
+                    </button>
+                </ul>
+            </div>
+        </nav>
+    </div>
     )
 }
 
