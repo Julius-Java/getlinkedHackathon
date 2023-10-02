b871c1aa723e4768fd71a46935802800c13dc332
Emmanuel Julius <emmanueljulius9102016@gmail.com>
2023-10-02 21:13:01 +0100

    Add favicon

--- components/shared/Layout.tsx
+++ components/shared/Layout.tsx
@@ -1,20 +1,26 @@
 import React from 'react'
 import Navbar from './Navbar'
 import Footer from './Footer'
+import Head from 'next/head'
 
 
 const Layout = ({children}: any) => {
     return (
-        <div className='-mr-[4px] sm:-mr-4 overflow-x-hidden'>
-            <header className='border-b border-b-slate-700 h-[10vh]'
-            >
-                <Navbar />
-            </header>
-            <main className=''>
-                {children}
-            </main>
-            <Footer />
-        </div>
+        <>
+            <Head>
+                <link rel="icon" href="/assets/hackImg2.png" sizes='32x32' />
+            </Head>
+            <div className='-mr-[4px] sm:-mr-4 overflow-x-hidden'>
+                <header className='border-b border-b-slate-700 h-[10vh]'
+                >
+                    <Navbar />
+                </header>
+                <main className=''>
+                    {children}
+                </main>
+                <Footer />
+            </div>
+        </>
     )
 }
 
